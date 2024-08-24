b988b5a6768f30c870b3b01d59ac85faf82c9acb
sarveshhome <sarvesh.carlton@gmail.com>
2024-08-24 16:17:57 +0530

    added consumer and publisher

--- obj/Debug/net8.0/dotNetCoreWebAPIWithMongoDB.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotNetCoreWebAPIWithMongoDB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotNetCoreWebAPIWithMongoDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18bd2acd3635f13a7b706c53e4efef906d9441f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotNetCoreWebAPIWithMongoDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotNetCoreWebAPIWithMongoDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net8.0/dotNetCoreWebAPIWithMongoDB.MvcApplicationPartsAssemblyInfo.cs
+++ obj/Debug/net8.0/dotNetCoreWebAPIWithMongoDB.MvcApplicationPartsAssemblyInfo.cs
@@ -10,6 +10,8 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("KafkaConsumerService")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("KafkaPublisherService")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.OpenApi")]
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
